aa0099198e3e4096dba2388f76e15696ce3c4f9f
kuma <kuma@mailfence.com>
2024-05-11 14:33:10 -0600

    Removed unnecessary constants

--- src/constants.ts
+++ src/constants.ts
@@ -5,31 +5,49 @@ const MAP_CONS = {
   a: 6378137, // NAD83 ellipsoid
   e: 0.0818191910428,
   e2: 0.006694380022898204, // NAD83 ellipsoid
-  e4: 4.4814723890978554e-05,
-  e6: 3.000067923474657e-07,
+  e4: 4.4814723890978554e-5,
+  e6: 3.000067923474657e-7,
   ep2: 0.006739496788262,
   ko: 0.9996, // UTM constant
+  lccCentralMer: -91.8666666666667,
+  lccLatOrig: 63.390675,
+  lccLat1: 49,
+  lccLat2: 77,
 };
 
 // app constants
 const APP_CONS = {
-  canvasWidth: 600, // canvas width in pixels
-  canvasHeight: 504, // canvas height in pixels // height/width = 1.19
-  padding: 0, // padding in pixels 30
-  scaleCoeff: 1.8, // scale coefficient 0.8
+  canvasWidth: 540, // canvas width in pixels
+  canvasHeight: 480, // canvas height in pixels
+  scaleCoeff: 0.00009,
+  geogCoordSize: 116, //115
+  padding: 20,
+  lonRange: [-141, -133, -125, -117, -109, -101, -93, -85, -77, -69, -61, -53],
+  latRange: [43, 51, 59, 67, 75, 83]
 };
 
 // boundary constants
 const BOUND_CONS = {
-  minLon: -144, //-141.0023496000000023,
-  minLat: 0, //41.6765555999999719,
-  maxLon: -54, //-52.6728133999999955,
-  maxLat: 90 //83.1188918999999942,
+  minLon: -141,
+  minLat: 41,
+  maxLon: -53,
+  maxLat: 84,
 };
 
-// canvas center shift coords
-const CANVAS_CONS = {
-
+// indicatrix colors
+const IDX_COLORS = {
+  40: "#a6cee3",
+  41: "#1f78b4",
+  42: "#b2df8a",
+  43: "#33a02c",
+  44: "#fb9a99",
+  45: "#e31a1c",
+  46: "#fdbf6f",
+  47: "#ff7f00",
+  48: "#cab2d6",
+  49: "#6a3d9a",
+  50: "#ffff99",
+  51: "#b15928"
 };
 
-export { MAP_CONS, APP_CONS, BOUND_CONS };
+export { MAP_CONS, APP_CONS, BOUND_CONS, IDX_COLORS };
